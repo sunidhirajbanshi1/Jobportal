10a31fd66bcb26f856f043b2d68708443e39e6f4
sunidhirajbanshi1 <sunidhirajbanshi222@gmail.com>
2024-11-28 18:54:46 +0545

    Add files via upload

---
+++ indexNavbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<title></title>
+<head>
+  <link rel ="stylesheet" href="style.css">
+</head>
+<div class="navbar">
+<div class="logo">
+            <img src="logo.png" alt="Logo">
+            <span>JOBS</span>
+        </div>
+       
+      
+        <nav>
+          
+          <a href="index1.php">Home</a> 
+             <a href="Aboutus.html">About Us</a>
+             <a href="contact.html">Contact Us</a>
+            <a href="jobseekerlogin.php">Jobseekers</a>
+            <a href="employerlogin.php">Employers</a>
+            <a href="adminlogin.php">Admin</a>
+          
+          <!-- <li><a href="">About Us</a></li> -->
+          <!-- <li><a href="">Contact Us</a></li> -->
+
+      </nav>
+    </div>
+
+   
+  
